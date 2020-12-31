906c0f02c0c101d0fc214cb23071c01fc4fc4c7b
devin12 <1400902533@qq.com>
2020-12-31 18:02:15 +0800

    log4j

--- src/main/java/cn/java/money/log4j/Log4jTest.java
+++ src/main/java/cn/java/money/log4j/Log4jTest.java
@@ -2,19 +2,23 @@ package cn.java.money.log4j;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.helpers.LogLog;
 import org.junit.jupiter.api.Test;
 
 public class Log4jTest {
 
     @Test
     public void testQuick() {
+        //开启log4j内置的日志记录,可以查看log4j自身代码中的日志
+        //LogLog.setInternalDebugging(true);
+
         /*
             如果没有以下初始化配置，出现如下错误；
             log4j:WARN No appenders could be found for logger (cn.java.money.log4j.Log4jTest).
             log4j:WARN Please initialize the log4j system properly.
          */
         // 初始化系统配置，不需要配置文件
-        BasicConfigurator.configure(); // 这里使用代码方式配置
+        //BasicConfigurator.configure(); // 这里使用代码方式配置 log4j.prorerties
         // 创建日志记录器对象
         Logger logger = Logger.getLogger(Log4jTest.class);
         // 日志记录输出
@@ -27,5 +31,10 @@ public class Log4jTest {
         // log4j 默认日志级别debug
         logger.debug("debug"); // 调试信息，一般在开发阶段使用，记录程序的变量、参 数等
         logger.trace("trace");   // 追踪信息，记录程序的所有流程信息
+
+        // 测试按照大小分隔
+        /*for (int i = 0; i < 100000; i++) {
+            logger.info("寒蝉凄切，对长亭晚，骤雨初歇。都门帐饮无绪，留恋处，兰舟催发。执手相看泪眼，竟无语凝噎。念去去，千里烟波，暮霭沉沉楚天阔。多情自古伤离别，更那堪，冷落清秋节！今宵酒醒何处？杨柳岸，晓风残月。此去经年，应是良辰好景虚设。便纵有千种风情，更与何人说？");
+        }*/
     }
 }
